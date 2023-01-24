ac645861ca0c5bfce3799f55e06769556a904acc
Clarkden <63682937+Clarkden@users.noreply.github.com>
2023-01-23 17:18:42 -0800

    Minor Update

--- components/navigations.js
+++ components/navigations.js
@@ -12,15 +12,15 @@ export default function Navigation({ navbarcolor, offsetY }) {
         <div className={`${navbarcolor} duration-300 z-40 sticky top-0`}>
             <nav className={`flex flex-row items-center md:items-baseline justify-between w-11/12 h-[4vh] sm-h[8vh] md:h-[10vh] mx-auto py-8 md:py-5`}>
                 <div className='relative group'>
-                    <h1 className='font-semibold text-base small:text-lg md:text-xl text-white'>CLARK HOLDEN</h1>
+                    <h1 className='font-semibold text-xl md:text-2xl text-white'>CLARK HOLDEN</h1>
                     {offsetY < 700 ? <div className='opacity-0 group-hover:opacity-100 top-10 absolute saturate-150 bg-green-300 p-2 transition-all w-[92vw] md:w-auto rounded-md md:rounded-full md:whitespace-nowrap'>
                         <p className='text-neutral-900 font-bold text-sm'>Stop looking at my name and check out my portfolio!</p>
                     </div> : null}
                 </div>
-                <div className='flex flex-row md:gap-2 transition'>
-                    <div className='flex flex-row gap-2 items-center ml-2'>
-                        <a target="_blank" rel="noopener norefferer" href="https://twitter.com/clarkholden5?lang=en"><FontAwesomeIcon icon={faTwitter} className='text-white hover:scale-110 hover:cursor-pointer' /></a>
-                        <a target="_blank" rel="noopener noreferrer" href='https://www.instagram.com/clarkh_/'><FontAwesomeIcon icon={faInstagram} className='text-white hover:scale-110 hover:cursor-pointer' /> </a>
+                <div className='flex flex-row gap-2 items-center justify-between'>
+                    <div className='flex flex-row gap-4 items-center ml-2'>
+                        <a target="_blank" rel="noopener norefferer" href="https://twitter.com/clarkholden5?lang=en"><FontAwesomeIcon icon={faTwitter} className='text-white hover:scale-110 hover:text-green-300 saturate-200 transition hover:cursor-pointer text-2xl' /></a>
+                        <a target="_blank" rel="noopener noreferrer" href='https://www.instagram.com/clarkh_/'><FontAwesomeIcon icon={faInstagram} className='text-white hover:scale-110 hover:text-blue-300 hover:cursor-pointer text-2xl' /> </a>
                     </div>
                 </div>
                 
